fb05cf1050c056ec92686ef23e6f016ebd1e747b
Ulicqeldroma <ulicqeldroma@gmail.com>
2017-12-20 20:07:11 -0600

    Added reprompt to historia.py. Added the model in a txt file

--- historia.py
+++ historia.py
@@ -11,7 +11,7 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 @ask.launch
 def new_history():
     welcome_msg = render_template('first')
-    return question(welcome_msg)
+    return question(welcome_msg).reprompt("Sorry I didn't get that. Are you a boy or a girl?")
 
 @ask.intent("Segundo", convert={'gender':str})
 def firstPart(gender):
@@ -21,7 +21,7 @@ def firstPart(gender):
 	else :
 		fmsg = render_template('boy')
 	session.attributes['gender'] = py_gender
-	return question(fmsg)
+	return question(fmsg).reprompt("Sorry I didn't get that. What will you choose, Cotton Candy? Chocolate? Popsicles?")
 
 @ask.intent("Tercero", convert={'candy':str})
 def secPart(candy):
@@ -29,7 +29,7 @@ def secPart(candy):
 	session.attributes['candy'] = py_candy
 	py_gender = session.attributes['gender']
 	sec_part = render_template('selection', gender=py_gender, candy=candy)
-	return question(sec_part)
+	return question(sec_part).reprompt("Sorry I didn't get that. Did I ate them all or did I save some?")
 
 @ask.intent("Cuarto", convert={'hunger':str})
 def terPart(hunger):
